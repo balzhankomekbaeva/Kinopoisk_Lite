74080a434612024afd3cea8228ef172364adf537
balzhankomekbaeva <230895@astanait.edu.kz>
2025-06-24 20:21:18 +0500

    Initial commit

---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Controllers\HomeController;
+use App\Controllers\MovieController;
+use App\Kernel\Router\Route;
+
+return [
+
+    Route::get('/home', [HomeController::class, 'index']),
+    Route::get('/movies', [MovieController::class, 'index']),
+
+];
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Kernel\App;
+
+define('APP_PATH', dirname(__DIR__));
+require_once APP_PATH.'/vendor/autoload.php';
+
+$app = new App;
+$app->run();
